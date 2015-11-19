676946e7bee27d9aaca350fdd99f25ad1114560c
andela-ydaniju <yusuf.daniju@andela.com>
2015-11-20 00:15:44 +0100

    All tests working for choice

---
+++ spec/choice_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+# Test for Choice class
+module Hangman
+  describe Choice do
+    context 'pick_word' do
+      picked_word = Choice.new.pick_word
+      it 'returns a string in downcase' do
+        expect(picked_word.class).to eql String
+      end
+      it 'cannot be an empty string' do
+        expect(picked_word.length).not_to eql 0
+      end
+    end
+
+    context 'show_word' do
+      showed_word = Choice.new.show_word
+      it "returns '_ ' in place of a letter in a word" do
+        expect(showed_word.class).to be String
+      end
+    end
+  end
+end
