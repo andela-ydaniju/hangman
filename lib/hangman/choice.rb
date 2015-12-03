964e620929144d946f8250f5693c40b748113a45
andela-ydaniju <yusuf.daniju@andela.com>
2015-12-03 12:39:50 +0100

    wrote first spec on game.rb

--- lib/hangman/choice.rb
+++ lib/hangman/choice.rb
@@ -3,16 +3,12 @@ module Hangman
   # This class serves words from file and tracks their display
   class Choice
     def initialize
-      read_file './lib/hangman/5desk.txt'
-    end
-
-    def read_file(dictionary)
-      @@word_array ||= File.readlines(dictionary)
+      @dictionary = File.readlines('./lib/hangman/5desk.txt')
     end
 
     def pick_word
       loop do
-        choice = @@word_array.sample.chomp.downcase
+        choice = @dictionary.sample.chomp.downcase
         break(choice) if choice.length > 5 && choice.length < 12
       end
     end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,18 +3,26 @@ Coveralls.wear!
 require 'spec_helper'
 # Test for Game class
 module Hangman
+  hangman_play = Hangman::Play.new
+  hangman_choice = Hangman::Choice.new
   describe Game do
-    before do
-      $stdin = StringIO.new('a')
-    end
-
-    after do
-      $stdin = STDIN
-    end
-    # context 'wrong_entry' do
-    #   it 'increment wrong_count' do
-    #     expect("farm".wrong_entry('b')).to
-    #   end
+    # before do
+    #   $stdin = StringIO.new('a')
     # end
+
+    # after do
+    #   $stdin = STDIN
+    # end
+    
+    context '#method_calls' do
+      it 'receive right method calls' do
+        allow(hangman_play).to receive(:word).and_return(true)
+        allow(hangman_play).to receive(:wrong_count).and_return(true)
+        allow(hangman_play).to receive(:right_guess).and_return(true)
+        allow(hangman_play).to receive(:total_lives).and_return(true)
+        allow(hangman_play).to receive(:play).and_return(true)
+        allow(hangman_play).to receive(:display).and_return(true)
+      end
+    end
   end
 end
