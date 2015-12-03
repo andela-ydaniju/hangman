6074dd46d6d1acf1c0c678f8706bc4910138116c
andela-ydaniju <yusuf.daniju@andela.com>
2015-12-03 13:03:18 +0100

    changed directory for 5desk.txt

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,16 +13,23 @@ module Hangman
     # after do
     #   $stdin = STDIN
     # end
-    
+
     context '#method_calls' do
-      it 'receive right method calls' do
+      it 'receives right method calls' do
         allow(hangman_play).to receive(:word).and_return(true)
         allow(hangman_play).to receive(:wrong_count).and_return(true)
         allow(hangman_play).to receive(:right_guess).and_return(true)
         allow(hangman_play).to receive(:total_lives).and_return(true)
         allow(hangman_play).to receive(:play).and_return(true)
         allow(hangman_play).to receive(:display).and_return(true)
+        allow(hangman_play).to receive(:start!).and_return('wibble')
+      end
+      it 'rejects wrong method calls' do
+        allow(hangman_play).to receive(:yusuf).and_return(false)
+        allow(hangman_play).to receive(:failure).and_return(false)
       end
     end
+
+    context ''
   end
 end
