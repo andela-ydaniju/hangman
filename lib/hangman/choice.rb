58bdeeab5ad2ca1a5de14a6411a979d91131900d
andela-ydaniju <yusuf.daniju@andela.com>
2015-12-03 11:21:04 +0100

    changed choice file

--- lib/hangman/choice.rb
+++ lib/hangman/choice.rb
@@ -3,18 +3,18 @@ module Hangman
   # This class serves words from file and tracks their display
   class Choice
     def initialize
-      @file_content = File.readlines('./lib/hangman/5desk.txt')
+      read_file './lib/hangman/5desk.txt'
     end
 
-    def pick_word(dictionary)
+    def read_file(dictionary)
+      @@word_array ||= File.readlines(dictionary)
+    end
+
+    def pick_word
       loop do
         choice = dictionary.sample.chomp.downcase
         break(choice) if choice.length > 5 && choice.length < 12
       end
-    end
-
-    def dictionary
-      @file_content
     end
   end
 end
--- lib/hangman/version.rb
+++ lib/hangman/version.rb
@@ -1,4 +1,4 @@
 # Version of gem
 module Hangman
-  VERSION = '0.1.2'
+  VERSION = '0.1.4'
 end
