373af7617e2434d87958e26d7193158d1ed50a86
andela-ydaniju <yusuf.daniju@andela.com>
2015-12-03 11:04:18 +0100

    changed choice file

--- lib/hangman/choice.rb
+++ lib/hangman/choice.rb
@@ -3,14 +3,18 @@ module Hangman
   # This class serves words from file and tracks their display
   class Choice
     def initialize
-      @dictionary = File.readlines('./lib/hangman/5desk.txt')
+      @file_content = File.readlines('./lib/hangman/5desk.txt')
     end
 
-    def pick_word
+    def pick_word(dictionary)
       loop do
-        choice = @dictionary.sample.chomp.downcase
+        choice = dictionary.sample.chomp.downcase
         break(choice) if choice.length > 5 && choice.length < 12
       end
+    end
+
+    def dictionary
+      @file_content
     end
   end
 end
