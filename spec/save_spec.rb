f3dddcbfd46415932d1b471cffaa3040531a0709
andela-ydaniju <yusuf.daniju@andela.com>
2015-12-11 01:47:58 +0100

    add tests for file

--- spec/save_spec.rb
+++ spec/save_spec.rb
@@ -3,42 +3,45 @@ Coveralls.wear!
 require 'spec_helper'
 # Test for Save class.
 module Hangman
-  describe Save do
-    include FakeFS::SpecsHelper
+  game = Game.new
+  save = Save.new(game)
 
-    it "should return a file with its content having a line length of 140 characters" do
-      # This is like the first safety check. Run this before your File Operations
-      FakeFS.activate!
-
-      # This is something special, it will take a while to load everytime you run
-      # the test, so don't panic if you're wondering why it appears that the test
-      # is "stuck". This stubs your existing Filesystem.
-      FakeFS::FileSystem.clone(Rails.root)
-
-      file = generate_file_and_return_filename('magic_happens_here')
-
-      File.readlines(file).each do |line|
-        expect(line.length).to eq(141)
-      end
-
-      FakeFS.deactivate!
+  context '#initialize' do
+    it 'is an instance of Save' do
+      expect(save.class).to be Save
     end
-    game = Game.new
-    save = Save.new(game)
+  end
 
-    context '#initialize' do
-      it 'is an instance of Save' do
-        expect(save.class).to be Save
-      end
+  context '#method_calls' do
+    it 'return true to these calls' do
+      allow(game).to receive(:word).and_return(true)
+      allow(game).to receive(:wrong_count).and_return(true)
+      allow(game).to receive(:right_guess).and_return(true)
+      allow(game).to receive(:wrong_entry).and_return(true)
     end
+  end
 
-    context '#method_calls' do
-      it 'return true to these calls' do
-        allow(game).to receive(:word).and_return(true)
-        allow(game).to receive(:wrong_count).and_return(true)
-        allow(game).to receive(:right_guess).and_return(true)
-        allow(game).to receive(:wrong_entry).and_return(true)
-      end
+  context '#load_data' do
+    it 'should load yaml' do
+      yaml_text = <<-EOF
+                    A_NAME: ABC
+                    A_ALIAS: my_alias
+                  EOF
+      yaml = YAML.load(yaml_text)
+      filepath = 'whatever.yml'
+      YAML.stub(:load_data).with(filepath).and_return(yaml)
+    end
+  end
+
+  context '#save_data' do
+    it 'should save to yaml' do
+      yaml_text = <<-EOF
+                    A_NAME: ABC
+                    A_ALIAS: my_alias
+                  EOF
+      yaml = YAML.dump(yaml_text)
+      filepath = 'whatever.yml'
+      YAML.stub(:save_data).with(filepath).and_return(yaml)
     end
   end
 end
